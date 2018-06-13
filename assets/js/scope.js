a4cba9eb4bebafd7439aceeb6a18ba3b233e91ad
MoyArt <moyart@gmail.com>
2018-06-13 17:01:56 -0500

    Added array and for loop to populate the HTML list

--- assets/js/scope.js
+++ assets/js/scope.js
@@ -1,1 +1,20 @@
 console.log('Scope module loaded');
+
+let mujiwara = ['Luffy', 'Zoro', 'Nami', 'Usopp', 'Nico Robin', 'Chopper', 'Frankie', 'Brook'];
+
+let list = document.getElementById('mujiwara-pirates');
+
+for(let i =0; i< mujiwara.length; i+=1){
+    // Every Mujiwara no ichimi of the list
+    let pirate = mujiwara[i];
+    //Lets create a list Element for every iteration.
+    let pirateElement = document.createElement('li');
+    //Now lets add a pirate from mujiwara Array
+    pirateElement.innerText = pirate;
+    //And finally we append our pirate to the DOM
+    list.appendChild(pirateElement);
+    addEventListener('click', (e)=>{
+        console.log(e);
+        console.log('I am ' + pirate + " From the Mujiwara Pirates!!");
+    })
+}
